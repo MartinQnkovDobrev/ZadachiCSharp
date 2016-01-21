a54bdf30fd8b8bede668ee8082b346f4dbdd0a78
martin <martin_qnkov_dobrev@abv.bg>
2016-01-21 20:48:50 +0200

    Another two

---
+++ PointInsideACircleAndOutsideARectangl/PointInsideACircleAndOutsideARectangl/Program.cs
@@ -0,0 +1,75 @@
+﻿//Write an expression that checks for given point (x, y) if it is within the circle K({1, 1}, 1.5) and out of the rectangle R(top=1, left=-1, width=6, height=2)
+
+
+using System;
+
+
+
+
+class PointInsideACircleAndOutsideARectangl
+{
+    static void Main()
+    {
+        Console.Write("Please enter Xc and Yc\n Xc = ");
+
+        float Xc = float.Parse(Console.ReadLine());
+
+        Console.Write(" Yc = ");
+
+        float Yc = float.Parse(Console.ReadLine());
+
+        double Radious = 1.5 * 1.5;
+        int CircleX = 1;
+        int CircleY = 1;
+
+
+        double PointsCoordinates = (CircleX - Xc) * (CircleX - Xc) + (CircleY - Yc) * (CircleY - Yc);
+        bool InCircle = Radious - PointsCoordinates >= 0;
+        bool InRectangle = Xc >= -1 && Xc <= 5 && Yc >= -1 && Yc <= 1;
+        if (InCircle && InRectangle)
+
+        {
+
+            Console.WriteLine("\nThe point wirh coordinates ( {0} : {1} ) is in the both figures!\n", Xc, Yc);
+
+        }
+
+        if (InCircle && !InRectangle)
+
+        {
+
+            Console.WriteLine("\nThe point wirh coordinates ( {0} : {1} ) is in the circle and out of the rectangle!\n", Xc, Yc);
+
+        }
+
+        if (!InCircle && InRectangle)
+
+        {
+
+            Console.WriteLine("\nThe point wirh coordinates ( {0} : {1} ) is in the rectangle and out of the cirlce!\n", Xc, Yc);
+
+        }
+
+        if (!InCircle && !InRectangle)
+
+        {
+
+            Console.WriteLine("\nThe point wirh coordinates ( {0} : {1} ) is outside both figures!\n", Xc, Yc);
+
+        }
+
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ TrapezoidArea/TrapezoidArea/Program.cs
@@ -0,0 +1,30 @@
+﻿
+//Write an expression that calculates trapezoid's area by given sides a and b and height h.
+
+
+using System;
+
+class TrapezoidArea
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter Side A of the trapezoid");
+        double A = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Enter side B of the trapezoid");
+        double B = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Enter the high (H) of the trapezoid");
+        double H = double.Parse(Console.ReadLine());
+
+
+
+        double Area = ((A + B) / 2.0) * H;
+
+        Console.WriteLine("The area of your trapezoid is :{0}",Area);
+        
+
+
+    }
+}
+
